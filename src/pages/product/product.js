e4c50ee9f533e408ab39b17cb21ec2f48e035766
chlyun <rkdmf1306@gmail.com>
2024-07-03 09:06:37 +0900

    14

--- src/pages/product/product.js
+++ src/pages/product/product.js
@@ -1,18 +1,31 @@
-import "@/pages/product/product.css";
-import { comma, insertLast, setDocumentTitle } from "kind-tiger";
-import getPbImageURL from "@/api/getPbImageURL";
-import gsap from "gsap";
-import pb from "@/api/pocketbase";
+import defaultAuthData from '@/api/defaultAuthData';
+import getPbImageURL from '@/api/getPbImageURL';
+import pb from '@/api/pocketbase';
+import '@/pages/product/product.css';
+import gsap from 'gsap';
+import {
+  comma,
+  getStorage,
+  insertLast,
+  setDocumentTitle,
+  setStorage,
+} from 'kind-tiger';
 
-setDocumentTitle("29CM / 상품목록");
+setDocumentTitle('29CM / 상품목록');
+
+if (!localStorage.getItem('auth')) {
+  setStorage('auth', defaultAuthData);
+}
 
 async function renderProductItem() {
-  const productData = await pb.collection("products").getFullList({
-    sort: "-created",
+  const productData = await pb.collection('products').getFullList({
+    sort: '-created',
   }); // SDK
 
   // const response = await tiger.get(`${import.meta.env.VITE_PB_API}/collections/products/records`) // Fetch API
   // const productsData = response.data.items;
+
+  const { isAuth } = await getStorage('auth');
 
   productData.forEach((item) => {
     const discount = item.price * (item.ratio * 0.01);
@@ -21,6 +34,7 @@ async function renderProductItem() {
       <li class="product-item">
           <div>
             <figure>
+              <a href="${isAuth ? `/src/pages/detail/index.html?product=${item.id}` : '/src/pages/login/'}"></a>
               <img src="${getPbImageURL(item)}" alt="" />
             </figure>
             <span class="brand">${item.brand}</span>
@@ -33,10 +47,9 @@ async function renderProductItem() {
           </div>
         </li>
     `;
-    insertLast(".container > ul", template);
+    insertLast('.container > ul', template);
   });
-
-  gsap.from(".product-item", { y: 30, opacity: 0, stagger: 0.1 });
+  gsap.from('.product-item', { y: 30, opacity: 0, stagger: 0.1 });
 }
 
 renderProductItem();
